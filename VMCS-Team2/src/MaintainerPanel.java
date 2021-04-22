edd61d17de974dd04100766f5f7e9490aba7fb63
aiswarya rajan <rajanaiswarya61@gmail.com>
2021-04-22 15:52:40 +0530

    GUI for verify password

--- VMCS-Team2/src/MaintainerPanel.java
+++ VMCS-Team2/src/MaintainerPanel.java
@@ -1,14 +1,83 @@
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 public class MaintainerPanel {
 
+	public static String passwordStatus="";
+	static MainVendingMachineControl vendingmachineControl= new MainVendingMachineControl();
+	
 public static void main(String [] args) {
-	Scanner s = new Scanner(System.in);
-	System.out.println("Enter Password");
-	String x = s.nextLine();
-	MainVendingMachineControl y = new MainVendingMachineControl();
-	y.verifyPassword(x);
+//	Scanner s = new Scanner(System.in);
+//	System.out.println("Enter Password");
+//	String x = s.nextLine();
+//	MainVendingMachineControl y = new MainVendingMachineControl();
+//	y.verifyPassword(x);
 	
+	start();
 	
+}
+
+public static void start() {
+	
+	JFrame frame = new JFrame();
+	frame.setSize(500,500);
+
+	
+	frame.setTitle("maintainer panel");
+	JPanel content=new JPanel();
+	frame.setContentPane(content);
+	
+//	content.setLayout(new GridLayout(10,0));
+	
+	JLabel label = new JLabel("Enter Password");
+            content.add(label);
+	
+    
+    
+	JTextField Password = new JTextField(5);
+	 Password.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			String password = Password.getText();
+			System.out.println(password);
+			passwordStatus=verifyPassword(password);
+			
+			System.out.println(passwordStatus);		
+			JLabel res = new JLabel("");
+	          content.add(res); 
+	          content.repaint();
+			}});
+      content.add(Password);
+      Password.setColumns(40);
+      content.repaint();
+      
+      
+      
+     
+
+      
+      
+//drinkssection
+	
+	frame.setVisible(true);
+	
+}
+public static String verifyPassword(String Password) {
+	
+	return(vendingmachineControl.verifyPassword(Password));
 }
 }
 	
