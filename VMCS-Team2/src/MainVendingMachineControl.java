750d48968242727185c429cc9d6f62af9922ca98
Ramya <ramyauma.ramya110@gmail.com>
2021-04-22 14:39:19 +0800

    Added Authentication Code

--- VMCS-Team2/src/MainVendingMachineControl.java
+++ VMCS-Team2/src/MainVendingMachineControl.java
@@ -1,4 +1,14 @@
-
 public class MainVendingMachineControl {
-
+	public static String savedPassword="1234";
+	public void verifyPassword(String password) {
+		if (password.equals(savedPassword)){
+			System.out.println("Valid Pass");
+		}
+		else {
+			System.out.println("Invalid");
+		}
+	
+	
+	}
 }
+
--- VMCS-Team2/src/MaintainerPanel.java
+++ VMCS-Team2/src/MaintainerPanel.java
@@ -1,4 +1,18 @@
-
+import java.util.Scanner;
 public class MaintainerPanel {
 
+public static void main(String [] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter Password");
+	String x = s.nextLine();
+	MainVendingMachineControl y = new MainVendingMachineControl();
+	y.verifyPassword(x);
+	
+	
 }
+}
+	
+	
+	
+	
+
