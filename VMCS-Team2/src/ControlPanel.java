c55f0303305e0d04d3d946c11f331799c67c6b01
Leslie <37355343+Leslie@users.noreply.github.com>
2021-04-23 11:59:31 +0800

    small modification

--- VMCS-Team2/src/ControlPanel.java
+++ VMCS-Team2/src/ControlPanel.java
@@ -1,4 +1,14 @@
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+ 
+import javax.swing.*;
 
 public class ControlPanel {
-
+	public static void controlGUI() {
+		
+	}
 }
